b35c88dc39f71e038a38305b25940c96b9030cbc
Alex <ztalexgor@gmail.com>
2020-05-28 21:16:43 +0300

    Объединил)

--- Lab 5/Lab 5/Lab 5.cpp
+++ Lab 5/Lab 5/Lab 5.cpp
@@ -11,6 +11,12 @@
 
 using namespace std;
 
+bool isNumber(string);
+float stringToFloat(string);
+queue<string> parseToTokens(string);
+queue<string> readFromFile(ifstream&);
+
+
 class Node {
 	string data;
 public:
@@ -58,7 +64,28 @@ public:
 	virtual Node* GetHead() {}
 };
 
-
+class StatementList {
+	map<string, float> statments;
+	vector<Tree*> StTrees;
+public:
+	StatementList(string file) {
+		ifstream fin;
+		fin.open(file);
+		string ForAdd;
+		//////////////////////Tut delenie na ryadki i IF'i || zapihivanie ih v vector/////////////////////
+	}
+	float GetNumber(string key) {
+		return statments[key];
+	}
+	void Count() {
+		for (int i = 0; i < StTrees.size(); i++) {
+			if (i = StTrees.size() - 1)
+				cout << "Result = " << StTrees[i] << ".";
+			else
+				statments[StTrees[i]->GetHead()->left->GetSymbol()] = StTrees[i]->Count(StTrees[i]->GetHead()->right);
+		}
+	}
+};
 
 class BinTree : public Tree {
 	bool IsState;
@@ -73,7 +100,7 @@ public:
 	}
 	float Count(Node* start) {
 		if (!start->left) {
-			if (start->isNumber)
+			if (start->isNumber())
 				return start->GetNumber();
 			return parent->GetNumber(start->GetSymbol());
 		}
@@ -116,26 +143,78 @@ public:
 	}
 };
 
-class StatementList {
-	map<string, float> statments;
-	vector<Tree*> StTrees;
-public:
-	StatementList(string file) {
-		ifstream fin;
-		fin.open(file);
-		string ForAdd;
-		//////////////////////Tut delenie na ryadki i IF'i || zapihivanie ih v vector/////////////////////
+
+
+
+int main() {
+	ifstream input("d:\\Учёба\\Файлы общего доступа\\KOD.txt");
+
+	queue<string> kod;
+	kod = readFromFile(input);
+	while (!kod.empty()) {
+		cout << kod.front() << endl;
+		kod.pop();
 	}
-	float GetNumber(string key) {
-		return statments[key];
+	return 0;
+}
+
+bool isNumber(string s) {
+	for (int i = 0; i < s.size(); i++) {
+		if (((int)s[i] < 48 || (int)s[i] > 57) && s[i] != '.')return false;
 	}
-	void Count() {
-		for (int i = 0; i < StTrees.size(); i++) {
-			if (i = StTrees.size() - 1)
-				cout << "Result = " << StTrees[i] << ".";
-			else
-				statments[StTrees[i]->GetHead()->left->GetSymbol()] = StTrees[i]->Count(StTrees[i]->GetHead()->right);
+	return true;
+}
+
+float stringToFloat(string s) {
+	float part1 = 0, part2 = 0;
+	int it = 0;
+	while (it < s.size() && s[it] != '.') {
+		part1 *= 10;
+		part1 += (int)s[it] - 48;
+		it++;
+	}
+	if (it != s.size()) {
+		for (int i = s.size() - 1; i > it; i--) {
+			part2 += (int)s[i] - 48;
+			part2 /= 10;
 		}
 	}
-};
+	return part1 + part2;
+}
 
+queue<string> parseToTokens(string s) {
+	queue<string> res;
+	string token = "";
+	for (int i = 0; i < s.size(); i++) {
+		string curr = "";
+		curr += s[i];
+		if (curr == "-" || curr == "+" || curr == "*" || curr == "/" || curr == "^" || curr == "=" || curr == "(" || curr == ")" || curr == " " || curr == "}" || curr == "{" || curr == ";") {
+			if (token != "")res.push(token);
+			if (curr != " ")res.push(curr);
+			token = "";
+		}
+		else {
+			token += curr;
+			/*if (token == "if") {
+			  res.push(token);
+			  token = "";
+			}*/
+		}
+	}
+	res.push(token);
+	return res;
+}
+
+queue<string> readFromFile(ifstream& inp) {
+	queue<string> kod, temp;
+	string s;
+	while (!inp.eof()) {
+		getline(inp, s);
+		temp = parseToTokens(s);
+		while (!temp.empty()) {
+			if (temp.front() != "")kod.push(temp.front());
+			temp.pop();
+		}
+	}
+	return kod;
+}
